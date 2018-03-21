643dd64cfb638391eabb035f658a7f788281fe72
czmccrory <caoilainnmccrory@gmail.com>
2018-03-21 21:53:28 +0000

    beginning of code (without people in meeting)

---
+++ src/Meeting.java
@@ -0,0 +1,76 @@
+/**
+ * @author caoilainnmccrory, joelsieber, patrickturton-smith, lucascerha
+ * 
+ * @version v1.0
+ */
+public class Meeting {
+	
+	private int startTime;
+	private int endTime;
+	private String description;
+	private String date;
+
+	public Meeting() {
+		startTime = 0;
+		endTime = 0;
+		description = "";
+		date = "";
+	}
+
+	/**
+	 * @return the startTime
+	 */
+	public int getStartTime() {
+		return startTime;
+	}
+
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+
+	/**
+	 * @return the endTime
+	 */
+	public int getEndTime() {
+		return endTime;
+	}
+
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public String getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
